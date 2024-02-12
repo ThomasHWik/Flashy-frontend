9ad1071dc1e0b6b874c56c9f29adef50b15414b5
Jørgen Texmo Grønn <jorgentg@stud.ntnu.no>
2024-02-12 15:01:01 +0100

    Resolve "create header"

---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/profile.css";
+
+function Profile(props) {
+    return (
+        <div>
+            <h1>p</h1>
+        </div>
+    )
+}
+
+export default Profile;
