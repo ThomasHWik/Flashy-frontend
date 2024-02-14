e675de382a6087800662c48629c860a0d8b191d0
Helene <helenban@stud.ntnu.no>
2024-02-14 09:47:45 +0100

    feature: create flashcard-deck

--- src/components/createFlashy.js
+++ src/components/createFlashy.js
@@ -1,35 +1,63 @@
 import { Checkbox, ToggleButton } from "@mui/material";
 import React from "react"
 import { useState } from "react"
-import "./CreateFlashy.css"
+import "./css/createFlashy.css"
 
 /*Få spørsmålene i rekkefølge i databasen og faktisk legge til input i databasen */
 
-function CreateFlashy(){
-    /*const [inputValue, setInputValue] = useState('');
-    function addQ(){
-            setInputValue();
+//lagre til array i react
+//done sender til databasen med fetch
 
-            const handleInputChange = (e) => {
-                setInputValue(e.target.value);
-              };
-    }*/
+function CreateFlashy() {
+    var questions = []
+    const [questionValue, setInputQuestion] = useState('');
+    const [answerValue, setInputAnswer] = useState('');
+    const [title, setTitle] = useState('');
+    function addQ() {
+        questions.push({ question: questionValue, answers: answerValue })
+        setInputQuestion();
+        console.log(questions)
+    }
+    const handleTitle = (e) => {
+        setInputTitle(e.target.value);
+    };
 
-    return (    
+    const handleInputQuestion = (e) => {
+        setInputQuestion(e.target.value);
+    };
+
+    const handleInputAnswer = (e) => {
+        setInputAnswer(e.target.value);
+    };
+
+    function sendFlashcard() {
+        const result = fetch("http://localhost:8080/flashcard/create",
+            {
+                method: "POST", body: JSON.springfy(
+                    { name: title, cards: questions, isprivate: 0 })
+            }
+        )
+        const status=JSON.parse(result)
+        console.log(status)
+    }
+    return (
         <div className="create">
-           <p className="title"><h2>Title: <input type="text" ></input></h2></p>
+            <p className="title" ><h2>Title: <input onChange={(e) => handleInputTitle(e)} type="text" ></input></h2></p>
             <p className="movetext">
-                <h3>Fill in question: <input type="text"></input></h3>
-                <h3>Fill in answer: <input type="textfield"></input> </h3>
+                <h3>Fill in question: <input onChange={(e) => handleInputQuestion(e)} type="text"></input></h3>
+                <h3>Fill in answer: <input onChange={(e) => handleInputAnswer(e)} type="text"></input> </h3>
             </p>
-            <p><button>Add new question</button>
-
-            <h3>Press for public carddeck <Checkbox></Checkbox></h3>
-
-            <button>Done</button>
-
+            <p>
+                <p className="button">
+                    <button onClick={() => addQ()}>Add new question</button>
+                </p>
+                <div className="public">
+                    <h3>Press for public carddeck <Checkbox></Checkbox></h3>
+                </div>
+                <p className="button2">
+                    <button onClick={() => sendFlashcard()}>Done</button>
+                </p>
             </p>
-            
         </div>
     )
 
