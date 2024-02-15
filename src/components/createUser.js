63b04db3b326eba3efe21ec0e10274a2d820f762
Thomas Haslund Wik <thomahw@stud.ntnu.no>
2024-02-15 13:58:48 +0100

    Resolve "connect login page to database"

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/createUser.css';
 
 function CreateUser() {
 
+    const [name, setName] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleUserName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setUserPassword(e.target.value)
+    }
+
+    async function sendCreateUser() {
+        const result = await fetch("http://localhost:8080/user/register",
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST", body: JSON.stringify(
+                {username: name, password: userPassword}
+            )
+        }
+        )
+        console.log({username: name, password: userPassword})
+        console.log(result)
+        console.log(result.status)
+        if (result.status == 200) {
+            localStorage.setItem("flashyUserName", name)
+            window.location.href = "/home"
+        } else {
+            setMessage("Invalid username")
+        }
+    }
+
     return (
         <div className="createUser">
             <h1>Create Flashy user</h1>
-            <p>Username: <input type="text"></input></p>
-            <p>Password: <input type="text"></input></p>
-            <button>Create User</button>
+            <p>Username: <input onChange={(e)=>handleUserName (e)} type="text"></input></p>
+            <p>Password: <input onChange={(e)=>handlePassword (e)} type="text"></input></p>
+            <button onClick={() => sendCreateUser()}>Create User</button>
+            <p>{message}</p>
             <a href="/login">
                 <button>Log in instead</button>
             </a>
--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/login.css';
 
 function Login() {
 
+    const [name, setName] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleUserName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setUserPassword(e.target.value)
+    }
+
+    // const handleMessage = (e) => {
+    //     setMessage(e.target.value)
+    // }
+
+    async function sendLogin() {
+        const result = await fetch("http://localhost:8080/user/login",
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST", body: JSON.stringify(
+                {username: name, password: userPassword}
+            )
+        }
+        )
+        console.log({username: name, password: userPassword})
+        console.log(result)
+        console.log(result.status)
+        if (result.status == 200) {
+            localStorage.setItem("flashyUserName", name)
+            window.location.href = "/home"
+            
+        } else {
+            setMessage("Wrong username or password :(")
+        }
+    }
+
     return (
         <div className="login">
             <h1>Flashy login</h1>
-            <p>Username: <input type="text"></input></p>
-            <p>Password: <input type="text"></input></p>
-            <button>Log in</button>
+            <p>Username: <input onChange={(e)=>handleUserName (e)} type="text"></input></p>
+            <p>Password: <input onChange={(e)=>handlePassword(e)} type="text"></input></p>
+            <button onClick={() => sendLogin()}>Log in</button>
+            <p>{message}</p>
             <a href="/createUser">
                 <button>Not user yet?</button>
             </a>
