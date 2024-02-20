2de95929b7b4d1b3e5255377666e89db6fb78de2
Katrine Mikalsen <katrimik@stud.ntnu.no>
2024-02-20 17:12:53 +0100

    create admin page and functionality

--- src/components/createAdmin.js
+++ src/components/createAdmin.js
@@ -1,11 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './css/createAdmin.css'; // Make sure the path is correct relative to this file
 
 function CreateAdmin() {
-    return (
-        <div>
-            <h1>hei</h1>
-        </div>
-    )
-}
 
-export default CreateAdmin;
+    const [name, setName] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    const [message, setMessage] = useState("")
+    const [adminUsers, setAdminUsers] = useState([])
+
+    const handleUserName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setUserPassword(e.target.value)
+    }
+
+    async function deleteAdmin(adminName){
+      const result = await fetch("http://localhost:8080/user/delete/"+adminName,
+      {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "bearer " +localStorage.getItem("flashyToken"),
+        },
+        method: "DELETE"
+    })
+
+    if(result.status == 200){
+      console.log(result.status)
+
+      fetchAdmins()
+    }
+    }
+
+    async function sendCreateAdmin() {
+        const result = await fetch("http://localhost:8080/user/admin/create",
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "bearer " +localStorage.getItem("flashyToken"),
+            },
+            method: "POST", body: JSON.stringify(
+                {username: name, password: userPassword}
+            )
+        }
+        )
+        console.log({username: name, password: userPassword})
+        console.log(result)
+        console.log(result.status)
+        const parsed = await result.json()
+        console.log(parsed)
+        if (result.status == 403) {
+          window.location.href = "/home"
+          
+        } else {
+            setMessage("Admin is created")
+        }
+    }
+
+    async function fetchAdmins() {
+      const result = await fetch("http://localhost:8080/user/admin/getall", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "bearer " +localStorage.getItem("flashyToken"),            
+        },
+        method: "GET",
+      });
+
+      console.log(result)
+      if (result.status == 200){
+        const admins = await result.json()
+        setAdminUsers(admins)
+        console.log(admins)
+      }
+    }
+
+    useEffect(() => {
+      fetchAdmins();
+    }, []);
+
+  return (
+    <div className="app">
+      <div className="admin-panel">
+        <div className="current-admin-users">
+          <h2>Current admin users</h2>
+          
+            {/* Mockup list items */}
+            {adminUsers.map((v,i) => (
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', position: 'relative'}}>
+              <p>{v} <button style={{position: 'absolute', right: '0' }}onClick={() => deleteAdmin(v)} aria-label="Delete">🗑️</button></p>
+            </div>
+          ))}
+            {/* <li> <button aria-label="Delete">🗑️</button></li>
+            <li>Admin User 2 <button aria-label="Delete">🗑️</button></li>
+            <li>Admin User 3 <button aria-label="Delete">🗑️</button></li> */}
+            {/* Add more list items as needed */}
+          
+        </div>
+        <div className="create-new-admin">
+          <h2>Create new admin</h2>
+            <label htmlFor="username">Username:</label>
+            <input onChange={(e)=>handleUserName (e)} type="text" id="username" name="username" />
+            <label htmlFor="password">Password:</label>
+            <input onChange={(e)=>handlePassword (e)} type="password" id="password" name="password" />
+            <button onClick={() => sendCreateAdmin()}>Create Admin</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAdmin
+
