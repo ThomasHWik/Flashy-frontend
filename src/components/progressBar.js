cfe9451a8c6335e0edb8b3490ee14f2be7af4e59
Aksel Fosaas <akselsf@stud.ntnu.no>
2024-03-18 12:37:23 +0100

    fixed progressbar + edit page

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,46 +1,20 @@
 import React from "react";
 import "./css/progressBar.css";
+import { useState } from "react";
 
-class ProgressBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { width: 0 };
-    }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.current !== this.props.current) {
-            this.animateWidthChange();
-        }
-    }
+function ProgressBar(props) {
 
-    animateWidthChange() {
-        const animationDuration = 500; // match this with your CSS transition duration
-        const frameDuration = 20; // how long each frame of the animation should last
-        const totalFrames = animationDuration / frameDuration;
-        const widthChangePerFrame = ((this.props.current + 1) / this.props.total * 100 - this.state.width) / totalFrames;
 
-        console.log(this.props.current);
 
-        let frame = 0;
-        const animateFrame = () => {
-            frame++;
-            this.setState(state => ({ width: state.width + widthChangePerFrame }));
 
-            if (frame < totalFrames) {
-                setTimeout(animateFrame, frameDuration);
-            }
-        };
 
-        animateFrame();
-    }
-
-    render() {
         return (
             <div className="progressContainer">
-                <div className="progressBar" style={{ width: `${this.state.width}%` }} />
+                <div className="progressBar" style={{ width: `${100 / props.total * (props.current+1)}%` }} />
             </div>
         );
-    }
+    
 }
 
 export default ProgressBar;
